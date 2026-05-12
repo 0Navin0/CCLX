891017769c0b1963d909d983a17294e649e80529
Navin Chaurasiya <nlc38@duke.edu>
2026-05-12 19:32:55 -0400

    multiple implementations
    1. Compute Phi(L) from CLF
    2. Implement M and logM based HOD model of Cacciato
    -- Comments --
    	* Should I change the Mass usage in CCL to work with logM?
    	* It should depend on the idea that if internally code performs
    	  a redundance operation of taking power of 10 in every
    	  iteration of calculation with halo mass function, bias and power specta,
    	  then definately we should change it to logM.
    3. Implemented the exact Cacciato+2013 samples and plotted HOD and
       Phi(L), where each sample is computed at its own mean redshift.
    4. I see that the Central galaxy's number density can vary between
       10-40% in the Cacciato samples.
    
    To dos:
    1. Implement threshold CLF and HODs
    2. get satellite fraction for both binned and threshold samples
    3. Update script.py with a working implementation of HOD model. For this
       you'll have to fully embed the CLF into power spectrum calculations,
       by modifying or creating a separate class in pyccl.halos.profiles.hod
       module.

--- my_work/script.py
+++ my_work/script.py
@@ -7,17 +7,34 @@ import matplotlib as mpl
 mpl.rcParams['font.size'] = 16
 mpl.rcParams['text.usetex'] = 'True'
 
+from typing import Union, Optional
+
 class CacciatoHOD(ccl.halos.HaloProfileHOD):
-    def __init__(self, c_M_relation,
+    def __init__(self, *, 
+                 mass_def: Union[str, ccl.halos.MassDef], 
+                 cM_rel: Union[str, ccl.halos.Concentration],
                  Lmin, Lmax, N_L=64,
                  lL_0=9.95, lM_1=11.24,
                  gamma_1=3.18, gamma_2=0.245,
                  sigma_c=0.157, alpha_s=-1.18,
                  b_0=-1.17, b_1=1.53*0.739, b_2=-0.217*0.739**2,
-                 fh=1., fs=1.):
-        # fh, fs the scaling of c_M_relation, to be include in _usat_real/fourier
+                 fh=1., fs=1.
+                 ) -> None:
+        """
+        Custom HOD model based on Cacciato et al.
+        
+        Args:
+            mass_def: Halo mass definition (e.g., '200m' or a MassDef object).
+            cM_rel: Concentration-mass relation (e.g., 'Duffy08' or a Concentration object).
+            **kwargs: Additional HOD parameters passed to the parent class.
+        """
+        # Initialize the parent class with essential parameters
+        # overwrite more parent attr if you need.
+        super().__init__(mass_def=mass_def, concentration=cM_rel)
+
+        # fh, fs the scaling of cM_rel, to be include in _usat_real/fourier
         # b_1 and b_2 have been rescaled by h from Cacciato et al. 2013
-        self.cM = c_M_relation
+        self.cM = cM_rel
 
         self.Lmin = Lmin
         self.Lmax = Lmax
@@ -36,9 +53,6 @@ class CacciatoHOD(ccl.halos.HaloProfileHOD):
         self.b_1 = b_1
         self.b_2 = b_2
         self.loge = 0.4342944819 # logarithm of e
-
-        # TODO: Do I need to do this?
-        super(ccl.halos.HaloProfileHOD, self).__init__()
 
     def _Lc(self, M):
         return self.L_0 * (M/self.M_1) ** self.gamma_1 /\
@@ -65,26 +79,31 @@ class CacciatoHOD(ccl.halos.HaloProfileHOD):
         return self._phi_star_s(M_use)/L_use * (L_use/self._L_star_s(M_use))**(self.alpha_s+1) * \
                np.exp(-(L_use/self._L_star_s(M_use))**2)
 
+    # updating the parent default HOD methods for specific usecase here
     def _Nc(self, M, a):
-        return simps(
+        return simpson(
             self.conditional_luminosity_function_central(self.L_arr, M),
             self.L_arr)
 
     def _Ns(self, M, a):
-        return simps(
+        return simpson(
             self.conditional_luminosity_function_satellite(self.L_arr, M),
             self.L_arr)
 
 if __name__=="__main__":
 
     cosmo = ccl.CosmologyVanillaLCDM()
-    
+
     # Here we use a mass definition with Delta = 200 times the matter density,
+    # Note: We are defining mass w.r.t the matter density, not the critical density, which is more common in some contexts. 
+    # keep in mind when instantiating base cl.halos.HaloProfileHOD class with mass_def and concentration-mass relation, as both should follow same rho_type convention.
+    # If they differ, cl.halos.HaloProfileHOD will raise an error, which is good to prevent mistakes.
     hmd_200m = ccl.halos.MassDef200m
-    # the Duffy 2008 concentration-mass relation,
-    cM = ccl.halos.ConcentrationDuffy08(hmd_200m)
-    
-    hod = CacciatoHOD(cM, 1.5e7, 5.6e9)
+    # The Duffy2008 concentration-mass relation, by default it will be initialized for S.O masses with Delta=Delta_vir of Delta_vir=200
+    # critical density based halo mass :math:`M_{200c}`.  (Evolution of Mvir (SO based mass even when Delta is fixed to 200) with redshift is shown below.)
+    cM = ccl.halos.ConcentrationDuffy08(mass_def=hmd_200m)
+
+    hod = CacciatoHOD(mass_def=hmd_200m, cM_rel=cM, Lmin=1.5e7, Lmax=5.6e9)
     mass_arr = np.geomspace(1e8, 1e16, 128)
     Ns = hod._Ns(mass_arr, 1.)
     Nc = hod._Nc(mass_arr, 1.)
